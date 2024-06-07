c8add4807271aef6edccc19b556ca75818c61ac6
salwakhairu <161412781+salwakhairu@users.noreply.github.com>
2024-06-07 20:26:00 +0700

    Delete go-crud-employee directory

--- go-crud-employee/controller/hello_world.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "net/http"
-
-func NewHelloWorldController() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	}
-}
--- go-crud-employee/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/Dryluigi/crud-employee-go
-
-go 1.21.5
-
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
